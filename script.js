dabe66f24c0de320b26c6293fef97ef9959674c3
Naren456 <narendrase666@gmail.com>
2025-01-06 10:45:32 +0530

    commi

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let addBtn = document.querySelector('.add-btn')
-let modal = document.querySelector('.modal-cont')
+let modalcont = document.querySelector('.modal-cont')
+let maincont = document.querySelector('.main-cont')
+let textArea = document.querySelector('.textArea-cont')
 let addBtnFlag = false
 addBtn.addEventListener('click',function(){
     addBtnFlag = !addBtnFlag
     if(addBtnFlag){
-        modal.style.display = 'flex'
+        modalcont.style.display = 'flex'
     }
     else{
-       modal.style.display = 'none'
+       modalcont.style.display = 'none'
+ }
+})
+function CreateTicket(task,id){
+    const TicketCont = document.createElement('div')
+    TicketCont.setAttribute('class' , 'ticket-cont')
+    TicketCont.innerHTML =`<div class="ticket-color"></div>
+             <div class="ticket-id">${id}</div>
+             <div class="task-area" >${task}</div>
+              <div class="ticket-lock">
+                <i class="fa-solid fa-lock"></i>
+              </div>`;
+              maincont.appendChild(TicketCont)
+    
+}
+modalcont.addEventListener('keydown',function(e){
+    if (e.key == "Shift"){
+      const task = textArea.value
+      const id =shortid();
+      CreateTicket(task,id)
+      modalcont.style.display = 'none'
+      addBtnFlag = false
     }
 })
