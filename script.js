ac181d38fbdcd88a2ed377f6bd0dbed0babd588d
Naren456 <narendrase666@gmail.com>
2025-01-06 11:19:06 +0530

    ticketcolor

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let maincont = document.querySelector('.main-cont')
 let textArea = document.querySelector('.textArea-cont')
 let listcolor = document.querySelectorAll('.priority-color')
 let addBtnFlag = false
+
+// modal task color//
+let modaltaskcolor = ""
 addBtn.addEventListener('click',function(){
     addBtnFlag = !addBtnFlag
     if(addBtnFlag){
@@ -13,10 +16,10 @@ addBtn.addEventListener('click',function(){
        modalcont.style.display = 'none'
  }
 })
-function CreateTicket(task,id){
+function CreateTicket(taskcolor,task,id){
     const TicketCont = document.createElement('div')
     TicketCont.setAttribute('class' , 'ticket-cont')
-    TicketCont.innerHTML =`<div class="ticket-color"></div>
+    TicketCont.innerHTML =`<div class="ticket-color" style="background-color:${taskcolor}"></div>
              <div class="ticket-id">${id}</div>
              <div class="task-area" >${task}</div>
               <div class="ticket-lock">
@@ -29,10 +32,20 @@ modalcont.addEventListener('keydown',function(e){
     if (e.key == "Shift"){
       const task = textArea.value
       const id =shortid();
-      CreateTicket(task,id)
+      CreateTicket(modaltaskcolor,task,id)
       modalcont.style.display = 'none'
       addBtnFlag = false
       textArea.value = ""
     }
 })
 
+//ticket color //
+listcolor.forEach(function(colorElement){
+  colorElement.addEventListener('click',function(){
+    listcolor.forEach(function(priorityColors){
+      priorityColors.classList.remove('active')
+    })
+    colorElement.classList.add('active')
+     modaltaskcolor = colorElement.classList[0]
+  })
+})
